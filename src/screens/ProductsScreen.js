f2f18bf9527f14c132aa6a43c3517c0eef08c170
Knbcharron <knbcharron@gmail.com>
2025-05-22 17:03:12 +0530

    src folder commiting

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,38 @@
+
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import products from '../data/products';
+const productsScreen=()=>{
+    return(
+          <FlatList
+              data={products}
+              renderItem={({item})=>(
+                <View style={styles.itemContainer}>
+                  <Image source={{uri:item.image}}
+             style={styles.image}/>
+              </View>
+              )}
+              numColumns={2}
+              />
+           
+    );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer:
+{
+  width:'50%',
+  padding:1
+},
+  image:
+  {
+    width:"100%",
+    aspectRatio:1,
+
+  }
+});
+export default productsScreen;
